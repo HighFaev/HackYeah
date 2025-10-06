493dee50767f28c71c49489b98329f8785fbb701
Karina <rudenko344@gmail.com>
2025-10-06 10:43:33 +0200

    upgrade links

--- result/js/script.js
+++ result/js/script.js
@@ -1,36 +1,3 @@
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.BlackBerry() ||
-            isMobile.iOS() ||
-            isMobile.Opera() ||
-            isMobile.Windows()
-        );
-    }
-};
-
-if (isMobile.any()) {
-    document.body.classList.add("_touch");
-}else{
-    document.body.classList.add("_pc");
-}
-
 //Burger ==========================================================
 const iconMenu = document.querySelector(".menu-header__icon");
 if(iconMenu){
@@ -42,80 +9,7 @@ if(iconMenu){
         menuBody.classList.toggle("active");
     });
 }
-//=================================================================
 
-
-//Tab switch ======================================================
-document.addEventListener("DOMContentLoaded", function () {
-  const tabButtons = document.querySelectorAll(".tab__navitem a");
-  const tabContents = document.querySelectorAll(".newsmedia__item");
-
-  tabButtons.forEach(button => {
-    button.addEventListener("click", function (e) {
-      e.preventDefault(); // stop the page from jumping
-
-      // remove active from all buttons
-      tabButtons.forEach(btn => btn.classList.remove("active"));
-
-      // add active to the clicked one
-      this.classList.add("active");
-
-      // hide all tab contents
-      tabContents.forEach(content => content.style.display = "none");
-
-      // show the one that matches href (#tab_01, #tab_02, #tab_03)
-      const targetId = this.getAttribute("href");
-      const target = document.querySelector(targetId);
-      if (target) {
-        target.style.display = "block";
-      }
-    });
-  });
-
-  // trigger the first tab as default
-  if (tabButtons.length > 0) {
-    tabButtons[2].click();
-  }
-}); //(Create links to blocks and give this blocks id with links' href)
-//================================================================
-
-//Maps ===========================================================
-
-//const styleArray;
-
-function initMap() {
-  // Punkt startowy (Warszawa)
-  const centrum = { lat: 52.2297, lng: 21.0122 };
-
-  // Tworzymy mapę
-  const mapa = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: centrum,
-    //styles: styleArray
-  });
-
-  // Przykładowy marker
-  new google.maps.Marker({
-  position: { lat: 52.2297, lng: 21.0122 },
-  map: mapa,
-  title: "Warszawa - centrum",
-
-  icon: {
-  url: "../img/icons/marker.svg",           // ścieżka do obrazka
-  scaledSize: new google.maps.Size(24, 29), // szerokość, wysokość w px
-  //anchor: new google.maps.Point(20, 40)
-  }
-});
-
-}
-//Сайт https://snazzymaps.com/     (Стили)
-/*<script
-      async
-      defer
-      src="https://maps.googleapis.com/maps/api/js?key=YOURKEY&callback=initMap">
-    </script>
-*/ // Add in html AFTER main script
-// In a block where you need create a map add id="map". Don't forget style height and width!!!
 //===================================================================
 function getResultData() {
             // Metoda 1: Z localStorage
@@ -172,14 +66,14 @@ function getResultData() {
             }, 1500); // Symulacja opóźnienia ładowania
         });
 //<======================================================================================
-let value = 60;
+let value = +34;
 const val1 = "jjjj";
-const val2 = "j222";
+const val2 = "https://www.bbc.com/news/articles/cr5r76e127do";
 const val3 = "j333";
 
-document.getElementById("resSum").innerHTML = val1;
-document.getElementById("true").innerHTML = val2;
-document.getElementById("notTrue").innerHTML = val3;
+document.getElementById("resSum").innerHTML = "The Israel-Palestinian conflict is one of the longest-running and most violent disputes in the world, with its origins dating back more than a century. The conflict began in the early 20th century, when the British took control of Palestine, a region with an Arab majority and a Jewish minority...";
+//document.getElementById("true").innerHTML = val2;
+//ocument.getElementById("notTrue").innerHTML = val3;
 
 function setGaugeValue(pie, value) {
   pie.style.setProperty('--p', value);
